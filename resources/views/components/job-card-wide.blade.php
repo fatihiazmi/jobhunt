4a384a742f96eb13e51d6c34571a70ef6a762d76
fatihiazmi <70769424+fatihiazmi@users.noreply.github.com>
2024-05-14 12:54:06 +0800

    frontend setup and styling

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,18 @@
+<x-card class="flex gap-x-6">
+    <div>
+        <x-employer-logo :width=42/>
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="/" class="self-start text-sm text-gray-400">IPG Mediabrands</a>
+
+        <h3 class="group-hover:text-blue-600 transition-colors duration-500 font-bold text-xl mt-3">Senior Programmer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - Salary: RM5000 - RM10000</p>
+    </div>
+
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</x-card>
---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,16 @@
+<x-card class="flex flex-col text-center">
+    <div class="self-start text-sm">IPG Mediabrands</div>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-500">Senior Programmer</h3>
+        <p class="text-sm mt-4">Full Time - Salary: RM5000 - RM10000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag size="small">Tag</x-tag>
+            <x-tag size="small">Tag</x-tag>
+            <x-tag size="small">Tag</x-tag>
+        </div>
+
+        <x-employer-logo :width=42/>
+    </div>
+</x-card>
